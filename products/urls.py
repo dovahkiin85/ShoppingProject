f852bc4b66589004521816f185a8288d7cba662c
dovahkiin85 <ilia.ostadazim85@gmail.com>
2023-07-12 19:30:16 +0330

    CRUD complete

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('products/', views.products, name='products'),
-    path('products/add', views.addProducts, name='addProducts')
+    path('products/add', views.addProducts, name='addProducts'),
+    path('products/edit', views.editProducts, name='editProducts'),
+    path('products/edit/<int:id>/', views.edit_post, name='product-edit'),
+
 ]
--- products/views.py
+++ products/views.py
@@ -1,10 +1,19 @@
+import imp
+from turtle import back
+
+
+import requests
 from multiprocessing import context
+from wsgiref.util import request_uri
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Products
 from .forms import ProductForm
 from django.http import HttpResponseRedirect
+from django.shortcuts import render,redirect, get_object_or_404
+from django.contrib import messages
+from .models import Products
 
 
 def products(request):
@@ -37,3 +46,49 @@ def addProducts(request):
         form = ProductForm()
 
     return render(request, 'p_add.html', {'form': form})
+def editProducts(request):
+    myproducts = Products.objects.all().values()
+    template = loader.get_template('p_edit.html')
+    context = {
+    'myproducts': myproducts,
+  }
+    if request.method == "POST":
+        form = request.POST
+        p_id = form['p_id']
+        product = Products()
+        product.id = p_id
+        product.delete()
+    return HttpResponse(template.render(context, request))
+def edit_post(request, id):
+    form = ProductForm(request.POST)
+    req = request
+    post = get_object_or_404(Products, id=id)
+    if request.method == 'GET':
+        myproducts = Products.objects.all().values()
+        x = str(req).split(' ')
+        y = x[2].split('/')
+        z = y[3]
+
+        for i in myproducts:
+            if str(i['id']) == str(z):
+                myproduct = i
+        return render(request, 'product-edit.html', {'form': form, 'product':myproduct})
+    if request.method == "POST":
+        myproducts = Products.objects.all().values()
+        inde = 0
+        for x in myproducts:
+            if str(x['id']) == str(request.POST['p_id']):
+                i = Products.objects.all()[inde]
+                i.p_name= request.POST['p_name']
+                i.p_desc= request.POST['p_desc']
+                i.p_owner= request.POST['p_owner']
+                i.p_stock= request.POST['p_stock']
+                i.p_category= request.POST['p_category']
+                i.p_price= request.POST['p_price']
+                i.save()
+            inde = inde + 1
+        template = loader.get_template('p_edit.html')
+        context = {
+        'myproducts': myproducts,
+        }
+        return HttpResponse(template.render(context, request))
