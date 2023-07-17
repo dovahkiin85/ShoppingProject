9365962dede3ad6c946501c3a310791053f7f6c1
Minator-web <63640410+Minator-web@users.noreply.github.com>
2023-07-17 17:14:31 +0330

    first home page

--- members/views.py
+++ members/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+
 
 def members(request):
-    template = loader.get_template('myFirst.html')
-    return HttpResponse(template.render())
+    return render(request, 'First.html')
