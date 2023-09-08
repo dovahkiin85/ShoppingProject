f7b93383bc6cb5c2c1db064226983ffba9db1a12
dovahkiin85 <ilia.ostadazim85@gmail.com>
2023-09-08 17:46:45 +0330

    commit

--- shopProject/settings.py
+++ shopProject/settings.py
@@ -9,11 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+LOGIN_REDIRECT_URL = '/home'
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-7$^mf4lsxtyr&-*w^&ruy@v$1(eg*@h4id@g*0q1tv7$=9w5#s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'members',
     'products',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'shopProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- shopProject/urls.py
+++ shopProject/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', include('members.urls')),
     path('', include('products.urls')),
-
+    path('login/', auth_views.LoginView.as_view(), name='login.html'),
+    path('login/',include('home.urls')),
     path('admin/', admin.site.urls),
 ]
